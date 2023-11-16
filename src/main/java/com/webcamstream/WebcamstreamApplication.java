550743f731789a839d775bc527111d2efacf667b
john <jmwilliams0604@gmail.com>
2023-11-15 19:32:33 -0600

    Initial commit

---
+++ src/main/java/com/webcamstream/WebcamstreamApplication.java
@@ -0,0 +1,13 @@
+package com.webcamstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcamstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcamstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webcamstream/WebcamstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcamstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcamstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
